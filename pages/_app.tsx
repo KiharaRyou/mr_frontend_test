461b3671a1b7cb2d92d677c86a982b963ab1aa67
CarlosRyou <ryoukayuu@gmail.com>
2023-07-31 08:15:08 +1200

    added layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/global.css'
-import type { AppProps } from 'next/app'
+import '@/styles/global.css';
+import type { AppProps } from 'next/app';
+import { Layout, Space } from 'antd';
+
+const { Header, Content } = Layout;
+
+const headerStyle: React.CSSProperties = {
+  marginTop: 20,
+  height: 32,
+  lineHeight: '32px',
+  backgroundColor: '#F6F6F7'
+};
+
+const contentStyle: React.CSSProperties = {
+  padding: 10,
+  backgroundColor: '#FFF',
+};
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  
+  return <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
+            <Header style={headerStyle}></Header>
+            <Content style={contentStyle} >
+              <Component {...pageProps} />
+            </Content>  
+          </Space>    
 }
